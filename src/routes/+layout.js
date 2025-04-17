5cab5e7fa83a7192cae59fbf8e606e52e802a57b
EdvardJohansen <edvarddallj97@gmail.com>
2025-04-17 16:58:29 +0200

    added-addtional-logic-to-the-session-check-&-routing-in-+layout.svelte

---
+++ src/routes/+layout.js
@@ -0,0 +1,22 @@
+import { redirect } from "@sveltejs/kit";
+import { supabase } from "$lib/supabase.js";
+
+/** @type {import('./$types').LayoutLoad} */
+export async function load({ fetch, url }) {
+  const pathname = url.pathname;
+
+  const publicRoutes = ["/sign-up"];
+
+  const { data, error } = await supabase.auth.getSession();
+  const session = data.session;
+
+  if (!data.session && !publicRoutes.includes(pathname)) {
+    throw redirect(307, "/sign-up");
+  }
+
+  if (data.session && pathname === "/sign-up") {
+    throw redirect(307, "/home");
+  }
+
+  return {};
+}
